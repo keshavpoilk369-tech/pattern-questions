584ed1a682cb629baf6812147f1026e412945cab
keshavpoilk369-tech <keshavpoilk369@gmail.com>
2026-09-13 19:20:35 +0530

    Add files via upload

---
+++ Diomand_pattern.java
@@ -0,0 +1,34 @@
+package Aimaster;
+import java.util.Scanner;
+public class Diomand_pattern {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        //-->>upper half
+        for(int i = 1; i <= n; i++){
+            //-->> printing spaces
+            for(int j = 1; j <= n-i; j++){
+                System.out.print(" ");
+            }
+            //-->>
+            for(int j = 1; j <= 2*i-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //-->> lower half
+        for(int i = n; i >= 1; i--){
+            //-->> printing spaces
+            for(int j = 1; j <= n-i; j++){
+                System.out.print(" ");
+            }
+            //-->>
+            for(int j = 1; j <= 2*i-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
