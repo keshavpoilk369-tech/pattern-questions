25aa0cffa438713c276bcb4f309472fa033cff50
keshavpoilk369-tech <keshavpoilk369@gmail.com>
2026-09-14 09:25:15 +0530

    Add files via upload

---
+++ Hollow_rohmbus.java
@@ -0,0 +1,25 @@
+package Aimaster;
+import java.util.Scanner;
+public class Hollow_rohmbus {
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            //-->> printing spaces
+            for(int j = 1; j <= n-i; j++){
+                System.out.print(" ");
+            }
+            //-->> printing stars1
+            for(int j = 1; j <= n; j++){
+                if(i == 1 || i == n || j == 1 || j == n){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+
+        }
+    }
+}
